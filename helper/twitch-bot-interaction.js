d91f79c1daaa42fd324a35286d10bdab03a87809
1F24DCA <fstflr446712@gmail.com>
2021-10-11 13:55:25 +0900

    사상에 맞는 파일들로 코드 이동 및 코드 정리, 식별자 개선 등 리팩터링

--- helper/twitch-bot-interaction.js
+++
@@ -1,9 +0,0 @@
-import BotInteraction from './base/bot-interaction.js';
-
-export default class TwitchBotInteraction extends BotInteraction {
-    emote(text) {
-        if (text.match('^(?:Wink|Jookchang|Jeoleon|AwesomeFace)$')) text = `fstflr${text}`;
-
-        return text;
-    }
-}
